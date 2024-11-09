f9880f8ef1076f12c59262faaaf9b5a7eaf0ac9b
joao-rib <jonnasn@gmail.com>
2024-11-09 18:50:52 +0000

    2024.11.09 - Lots of abstract progress

--- Meal/eating.c
+++ Meal/eating.c
@@ -30,21 +30,21 @@ void	start_eating(t_table *tab)
 	phil_temp = tab->phil_list;
 	while (i != tab->num_philo) //WIP Será necessário função específica para apenas um filósofo
 	{
-		pthread_create(phil_temp->ph_thread, NULL, handle_spaghetti, phil_temp);
+		pthread_create(phil_temp->ph_thread, NULL, handle_spaghetti, phil_temp); //Considerar um handle_error
 		phil_temp = phil_temp->right;
 		i++;
 	}
-	pthread_create(tab->tab_thread, NULL, handle_table, tab);
+	pthread_create(tab->tab_thread, NULL, handle_table, tab); //Considerar um handle_error
 	tab->starting_time = get_time();
 	set_mtx_bool(&tab->tab_mutex, &tab->ready_to_start, true); //=all_thread_ready
 	phil_temp = tab->phil_list; //acho que já deu a volta...
 	i = 0;
 	while (i != tab->num_philo)
 	{
-		pthread_join(phil_temp->ph_thread, NULL);
+		pthread_join(phil_temp->ph_thread, NULL); //Considerar um handle_error
 		phil_temp = phil_temp->right;
 		i++;
 	}
 	set_mtx_bool(&tab->tab_mutex, &tab->ready_to_end, true); //=end_simulation
-	pthread_join(tab->tab_thread, NULL);
+	pthread_join(tab->tab_thread, NULL); //Considerar um handle_error
 }
